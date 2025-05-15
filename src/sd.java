80b733cd4b22f12acca30bc619c109d38a4cd307
xinyuheng <qw@123.com>
2025-05-15 19:50:31 +0800

    fourth commit

--- src/sd.java
+++ src/sd.java
@@ -1,7 +1,15 @@
+import java.awt.*;
+
 public class sd {
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
+        Frame frame=new Frame();
+        Panel panel=new Panel();
+        frame.setLayout(null);
+        frame.setBounds(300,300,500,500);
+        frame.setBackground(new Color(66, 152, 43));
+        panel.setBounds(50,50,400,400);
+        panel.setBackground(new Color(157, 50, 50));
+        frame.add(panel);
+        frame.setVisible(true);
     }
 }
