bd2ef49085dfabe556e77983df99e56079ff9e34
xinyuheng <qw@123.com>
2025-05-17 19:12:56 +0800

    fifth commit

--- src/sd.java
+++ src/sd.java
@@ -4,20 +4,29 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 public class sd {
     public static void main(String[] args) {
-      new windowframe();
+      new keyframe();
     }
 }
-class windowframe extends Frame{
-    public windowframe(){
-         setBounds(200,200,300,400);
-         setBackground(Color.red);
-         setVisible(true);
-         addWindowListener(new mywindowlistener());
+class keyframe extends Frame{
+    public keyframe(){
+        setBounds(200,200,500,500);
+        setVisible(true);
+        addKeyListener(new mykeylistener());
+        addWindowListener(new mywindowlistener());
     }
-    class mywindowlistener extends WindowAdapter{
-        @Override
-        public void windowClosing(WindowEvent e) {
-            System.exit(0);
-        }
+}
+class mykeylistener extends KeyAdapter{
+    @Override
+    public void keyPressed(KeyEvent e) {
+       int keycode=e.getKeyCode();
+       if(keycode==KeyEvent.VK_UP){
+           System.out.println("按下了上贱");
+       }
+    }
+}
+class mywindowlistener extends WindowAdapter{
+    @Override
+    public void windowClosing(WindowEvent e) {
+        System.exit(0);
     }
 }
